a1df0b6a8b026f78ba74d3f8e821d3cd98a3c24c
boatvanier <csvaniercourse@gmail.com>
2026-03-04 15:56:49 -0500

    Regex Lab 1-6

---
+++ src/regex/RegexLab.java
@@ -0,0 +1,28 @@
+package regex;
+
+public class RegexLab {
+//    1. Match exactly 5 digits
+//    \d{5}
+//    java: "^\\d{5}$"
+//
+//    2, Match a username
+//    [a-zA-z]\w{4,11}
+//    java: "^[a-zA-z]\\w{4,11}$"
+//
+//    3, Match words ending with "ing"
+//    \w+ing
+//    java: "^\\w+ing$"
+//
+//    4, Match dates YYYY-MM-DD
+//    \d{4}-\d{2}-\d{2}
+//    java: "^\\d{4}-\\d{2}-\\d{2}"
+//
+//    5, Password(at least 8 chars, 1 digit, 1 uppercase)
+//    (?=.*[A-Z](?=.*\d).*{8,})
+//    java: "^(?=.*[A-Z](?=.*\\d).*{8,})$"
+//
+//    6, Match hex color code
+//    #([A-Fa-f0-9]{6})
+//    java: "^#([A-Fa-f0-9]{6})$"
+
+}
