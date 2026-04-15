617448e45b0db9361791ebd8d5d9403c89af29f9
boatvanier <csvaniercourse@gmail.com>
2026-04-15 14:49:16 -0400

    static members

--- src/Car.java
+++ src/Car.java
@@ -2,20 +2,24 @@ public class Car {
     String brand;
     String color;
     int speed;
+    static int counter;
 
     public Car() {
+        counter++;
     }
 
     public Car(String brand, String color) {
         this.brand = brand;
         this.color = color;
         this.speed = 0;
+        counter++;
     }
 
     public Car(String brand, String color, int speed) {
         this.brand = brand;
         this.color = color;
         this.speed = speed;
+        counter++;
     }
 
     void displayInfo() {
@@ -23,5 +27,9 @@ public class Car {
     }
     void accelerate() {
         speed += 10;
+    }
+
+    static void showCounter() {
+        System.out.println("Totally " + counter + " cars are created.");
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -84,7 +84,6 @@ public class Main {
 //        System.out.println(numbers[3]);
 //
 
-        String a = new String();
         Car car1 = new Car();
         Car car2 = new Car();
         car1.brand = "Toyota";
@@ -96,31 +95,39 @@ public class Main {
         Car car3 = new Car("Mazda","red");
         Car car4 = new Car("Ford", "Blue", 130);
 
-//        System.out.println(car1.speed);
-//        System.out.println(car2.speed);
-//        car1.speed = 110;
+        System.out.println(car1.speed);
+        System.out.println(car2.speed);
+        car1.speed = 110;
+
+        System.out.println(car1.speed);
+        System.out.println(car2.speed);
+
+        car1.displayInfo();
+        car1.accelerate();
+        car1.displayInfo();
+
+        System.out.println(Car.counter);
+        Car.showCounter();
+
+//            Product p1 = new Product();
+//            p1.displayProduct();
+//            Product p2 = new Product("Camera", 3000);
+//            p2.displayProduct();
 //
-//        System.out.println(car1.speed);
-//        System.out.println(car2.speed);
+//            Customer c1 = new Customer("Jack", "jack@email.com");
+//            c1.displayCustomer();
+//            Customer c2 = new Customer();
+//            c2.displayCustomer();
+//            c2.name = "Rose";
+//            c2.email="rose@email.com";
+//            c2.displayCustomer();
+//
+//            Order o1= new Order(p2,c2,2);
+//            o1.displayOrder();
+//            Order o2 = new Order(p2);
+//            o2.displayOrder();
+//            Order o3 = new Order(p1, c2, 1);
 
-//        car1.displayInfo();
-//        car1.accelerate();
-//        car1.displayInfo();
-
-        Book book1;
-        book1 = new Book();
-        Book book2 = new Book("DB book", "Ada", 25);
-        book1.title = "Java book";
-        book1.author = "Thomas";
-        book1.price = 20;
-//        book2.title = "DB book";
-//        book2.author = "Ada";
-//        book2.price = 25;
-        book1.displayInfo();
-        book2.displayInfo();
-        book1.applyDiscount(0.2);
-        book1.displayInfo();
-        book2.displayInfo();
     }
 
 }
